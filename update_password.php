c01b59dc07fe2982fe87c5ee41d17b3c75dca4bf
9,15,14 <127332524+justine91514@users.noreply.github.com>
2026-05-07 22:58:07 +0800

    php mailer is downloded

--- update_password.php
+++ update_password.php
@@ -1,17 +1,24 @@
 <?php
 include 'includes/db.php';
 
-if (!isset($_POST['id'], $_POST['new'])) {
-    exit("invalid request");
-}
-
 $id = $_POST['id'];
 $new = $_POST['new'];
 
+$user = $conn->query("SELECT * FROM users WHERE id='$id'")->fetch_assoc();
+
+if (!$user) exit("User not found");
+
+// ❌ prevent same password
+if ($user['password'] === $new) {
+    exit("New password cannot be same as old password");
+}
+
 $conn->query("
     UPDATE users 
-    SET password = '$new'
-    WHERE id = '$id'
+    SET password='$new',
+        reset_token=NULL,
+        reset_expiry=NULL
+    WHERE id='$id'
 ");
 
 echo "success";
